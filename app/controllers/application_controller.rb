221f53d4e76c54320b4b33561abfd0a8f516bfe1
iamchacha021 <iamchacha021@gmail.com>
2023-02-26 20:06:24 +0300

    done

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < Sinatra::Base
+
+    get '/' do
+      'reload please!!!'
+    end
+  
+    get '/times/:num1/:num2' do
+      num1 = params[:num1].to_i
+      num2 = params[:num2].to_i
+  
+      product = num1 * num2
+  
+      {result: product}.to_json
+    end
+    
+  end
